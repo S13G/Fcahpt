7d48405c142ee63437356ff77267be7d108e9495
s13g <ayflix0@gmail.com>
2023-03-07 16:55:09 +0100

    deployment

--- car_rental/settings.py
+++ car_rental/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fcahpt.up.railway.app', '127.0.0.1']
 
 # Application definition
 
